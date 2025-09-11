4c49bc67c1c1e2796b91bb3bf27e1bcc2384e19b
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-09-10 23:05:44 -0400

    initial commit for get-course-content feature
    Co-authored-by: YousefH <yousef-0614@users.noreply.github.com>

---
+++ scripts/content.js
@@ -0,0 +1,25 @@
+console.log('Content script loaded');
+
+window.addEventListener("load", function() {
+    getCourseInformation();
+}, false); 
+
+function getBaseURL() {
+    return window.location.protocol + "//" + window.location.host;
+}
+
+async function getCourseInformation() {
+    try {
+        if (window.location.href.includes('/d2l/')){
+            let baseURL = getBaseURL();
+                const response = await fetch(baseURL + "/d2l/api/lp/1.43/enrollments/myenrollments/");
+                if (!response.ok) {
+                    throw new Error(`HTTP error. Status: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log(data);
+        }
+    } catch (error) {
+        console.error("Error fetching JSON:", error);
+    }
+}
