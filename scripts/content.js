13aa7190d301d42370fdd9ff762dfaf56da34655
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-09-11 22:44:36 -0400

    update Brightspace data fetching logic

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,46 @@
 console.log('Content script loaded');
 
 window.addEventListener("load", function() {
-    getCourseInformation();
+    getBrightspaceCourses();
 }, false); 
 
 function getBaseURL() {
     return window.location.protocol + "//" + window.location.host;
 }
 
-async function getCourseInformation() {
+// gets all pages of a paginated request from D2L Brightspace and returns combined data
+async function getBrightspaceData(url) {
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if ("Next" in data && data && data.Next) { // check if there is next page for course data
+        return data.Object.concat(await getBrightspaceData(data.Next));
+    }
+    else if ("PagingInfo" in data && data.PagingInfo && data.PagingInfo.HasMoreItems) { // check if there is next page for enrollment data
+        const currentPage = new URL(url);
+        currentPage.searchParams.set("bookmark", response.PagingInfo.Bookmark); //append ?bookmark=... for next page
+
+        const nextPageItems = await getBrightspaceData(currentPage.toString());
+        return data.Items.concat(nextPageItems);
+    }
+    return "Items" in data ? data.Items : data.Object; // if data has "Items" then return Items else return Object
+}
+
+async function getBrightspaceCourses() {
+
+    function isValidCourse(course) {
+        return course.Access.CanAccess && course.Access.IsActive && course.OrgUnit.Type.Id === 3;
+    }
+
     try {
         let baseURL = getBaseURL();
-            const response = await fetch(baseURL + "/d2l/api/lp/1.43/enrollments/myenrollments/");
-            if (!response.ok) {
-                throw new Error(`HTTP error. Status: ${response.status}`);
-            }
-            const data = await response.json();
-            console.log(data);
+        let coursesURL = baseURL + "/d2l/api/lp/1.43/enrollments/myenrollments/"
+        const allCourses = await getBrightspaceData(coursesURL);
+        const courses = allCourses.filter(isValidCourse);
+
+        console.log(courses);
+        
     } catch (error) {
         console.error("Error fetching JSON:", error);
     }
