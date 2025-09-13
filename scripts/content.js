f7184b6a5c77877ec331a692d379a9a803ad0611
yousef-0614 <133291362+yousef-0614@users.noreply.github.com>
2025-09-13 15:50:58 -0400

    Wrote getCourseContent.
    Co-authored-by: Cam <CamCatTay@users.noreply.github.com>

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,8 @@
 console.log('Content script loaded');
 
 window.addEventListener("load", function() {
-    getBrightspaceCourses();
+    //getBrightspaceCourses();
+    getCourseContent();
 }, false); 
 
 function getBaseURL() {
@@ -14,8 +15,12 @@ async function getBrightspaceData(url) {
     const response = await fetch(url);
     const data = await response.json();
 
-    if ("Next" in data && data && data.Next) { // check if there is next page for course data
-        return data.Object.concat(await getBrightspaceData(data.Next));
+    if ("Next" in data) { // check if there is next page for course data
+        if (!data.Next) {
+            return data.Objects;
+        } else {
+            return data.Objects.concat(await getBrightspaceData(data.Next));
+        }
     }
     else if ("PagingInfo" in data && data.PagingInfo && data.PagingInfo.HasMoreItems) { // check if there is next page for enrollment data
         const currentPage = new URL(url);
@@ -27,11 +32,11 @@ async function getBrightspaceData(url) {
     return "Items" in data ? data.Items : data.Object; // if data has "Items" then return Items else return Object
 }
 
-async function getBrightspaceCourses() {
+function isValidCourse(course) {
+    return course.Access.CanAccess && course.Access.IsActive && course.OrgUnit.Type.Id === 3;
+}
 
-    function isValidCourse(course) {
-        return course.Access.CanAccess && course.Access.IsActive && course.OrgUnit.Type.Id === 3;
-    }
+async function getBrightspaceCourses() {
 
     try {
         let baseURL = getBaseURL();
@@ -40,8 +45,46 @@ async function getBrightspaceCourses() {
         const courses = allCourses.filter(isValidCourse);
 
         console.log(courses);
+        return courses;
         
     } catch (error) {
         console.error("Error fetching JSON:", error);
     }
+}
+
+// gets the course ID of the given courses
+async function getCourseIDs(courses) {
+    const ids = [];
+    courses.forEach(function(course) {
+        ids.push(getCourseID(course));
+    });
+    return ids;
+}
+
+function getCourseID(courseData) {
+    return courseData.OrgUnit.Id;
+}
+
+// gets the content section of the courses
+async function getCourseContent () {
+
+    try {
+        let baseURL = getBaseURL();
+        const courses = await getBrightspaceCourses();
+        const courseIds = await getCourseIDs(courses);
+        const courseId = courseIds[0]; // for testing, just get the first course
+
+        const courseIdsCSV = courseIds.join(','); // joins the Ids into a CSV for the URL
+
+        let assignmentURL = baseURL + "/d2l/api/le/1.67/content/myItems/?startDateTime=null&endDateTime=null&orgUnitIdsCSV=" + courseIdsCSV;
+        
+        courseContent = await getBrightspaceData(assignmentURL);
+
+        console.log(courseContent);
+
+        return courseContent;
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+
 }
